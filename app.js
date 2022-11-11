21276fd8af232a1926bbf0609c2eb345270f71f3
konnovazhanna <konnovazhanna@gmail.com>
2022-11-11 11:37:11 +0300

    На switch-case

--- app.js
+++ app.js
@@ -275,3 +275,26 @@ console.log("За", k, "лет", k1, "високосных года");
 // } else {
 //   console.log('Неверно');
 // }
+
+// На switch-case
+// Переменная num может принимать 4 значения: 1, 2, 3 или 4. Если она имеет значение '1', то в переменную result запишем 'зима', если имеет значение '2' – 'весна' и так далее. Решите задачу через switch-case.
+
+const num = 3;
+
+switch (num) {
+  case 1:
+    console.log('Зима')
+    break;
+  case 2:
+    console.log('Весна')
+    break;
+  case 3:
+  console.log('Лето')
+  break;
+  case 4:
+  console.log('Осень')
+  break;
+  default:
+  console.log('')
+  break;
+}
